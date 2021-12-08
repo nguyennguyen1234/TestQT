11123d4dcf61d7aaf725d5b1710f72fd6f0c5cdc
RD320.FIT.UET <rd320.fit@gmail.com>
2021-12-08 19:52:06 +0700

    New type.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <QtGlobal>
 
-int testQt() {
+qint32 testQt() {
     quint32 a;
     return a+5;
 }
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    //QCoreApplication a(argc, argv);
 
-    return a.exec();
+    return 0;
 }
